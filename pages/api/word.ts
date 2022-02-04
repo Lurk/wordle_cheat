80ca22625b03ebf1572b7076fc8efc826614a05e
Sergey Bargamon <sergey@bargamon.ru>
2022-02-05 00:14:22 +0100

    fix 'The default export must be a function or server.' #9

---
+++ pages/api/word.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<string| {error: string}>) {
+  try {
+    const raw = await fetch("http://78.47.186.254/api/word", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req.body),
+    });
+    res.status(200).send(await raw.text());
+  } catch (err) {
+    res.status(500).json({ error: "failed to load data" });
+  }
+};
