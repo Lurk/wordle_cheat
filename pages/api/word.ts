65a699ab2f4d6bfba871448d78c57dab3955d546
Sergey Bargamon <sergey@bargamon.ru>
2022-02-04 23:55:57 +0100

    fix 'The default export must be a function or server.' #4

--- pages/api/word.ts
+++ pages/api/word.ts
@@ -3,15 +3,15 @@ import fetch from "node-fetch";
 
 export default async (req: NextApiRequest, res: NextApiResponse<string>) => {
   if (req.method === "POST") {
-    const raw = await fetch("http://78.47.186.254/api/word", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req.body),
-    });
-    res.status(200).send(await raw.text());
+    // const raw = await fetch("http://78.47.186.254/api/word", {
+    //   method: "POST",
+    //   headers: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(req.body),
+    // });
+    res.status(200).send("aeros");
   } else {
     res.status(400).send("");
   }
