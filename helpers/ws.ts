d95f575ff56cdd95a5fcaa455b63bfd9029b8054
Sergey Bargamon <sergey@bargamon.ru>
2022-02-12 19:46:58 +0100

    query api via ws

---
+++ helpers/ws.ts
@@ -0,0 +1,74 @@
+import WebSocket from "ws";
+import crypto from "crypto";
+import { Rules } from "./word";
+
+let connected = false;
+let lastQuery: false | Rules = false;
+const resolvers: Map<string, (w: string) => void> = new Map();
+const ws = new WebSocket("ws://127.0.0.1:8080/api/ws");
+
+interface Message {
+  id: string;
+  word: string;
+}
+
+function isMessage(v: any): v is Message {
+  return (
+    typeof v === "object" &&
+    !Array.isArray(v) &&
+    v !== null &&
+    v.hasOwnProperty("id") &&
+    v.hasOwnProperty("word")
+  );
+}
+
+function fromData(data: string): Message | false {
+  const message = JSON.parse(data);
+  return isMessage(message) ? message : false;
+}
+
+ws.onopen = function () {
+  console.log("connected");
+  connected = true;
+  if (lastQuery) {
+    send(lastQuery);
+    lastQuery = false;
+  }
+};
+
+ws.addEventListener("close", function () {
+  console.log("disconnected");
+  connected = false;
+});
+
+ws.addEventListener("error", function (event) {
+  console.log("WebSocket error: ", event);
+});
+
+ws.addEventListener("message", ({ data }) => {
+  const message = fromData(data.toString());
+  if (message) {
+    console.log(message);
+    const resolver = resolvers.get(message.id);
+    if (resolver) {
+      resolver(message.word);
+      resolvers.delete(message.id);
+    } 
+  }
+});
+
+export function send(rules: Rules): Promise<string> {
+  if (crypto.randomUUID) {
+    const message = { id: crypto.randomUUID(), rules };
+    if (connected) {
+      ws.send(JSON.stringify(message));
+    } else {
+      lastQuery = rules;
+    }
+    return new Promise((resolve) => {
+      resolvers.set(message.id, resolve);
+    });
+  } else {
+    throw new Error("crypto without uuid");
+  }
+}
--- pages/api/word.ts
+++ pages/api/word.ts
@@ -1,20 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { send } from "../../helpers/ws";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<string | { error: string }>
 ) {
   try {
-    const raw = await fetch("http://78.47.186.254/api/word", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req.body),
-    });
-    res.status(200).send(await raw.text());
+    res.status(200).send(await send(req.body));
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: "failed to load data" });
   }
 }
